64a38fa05b91c35af48a4aac09fbde1ae743cf6b
mustafaAwwal <mustafaawwal1@gmail.com>
2026-04-04 18:34:25 +0500

    Initial Commit

---
+++ design/designApp.swift
@@ -0,0 +1,17 @@
+//
+//  designApp.swift
+//  design
+//
+//  Created by MustafA Awwal on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
